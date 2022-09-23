e834ef5e29453d1ee24afbb75d5434053101e705
lenehrt <89034569+lenehrt@users.noreply.github.com>
2022-09-23 15:40:43 -0400

    testt

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,30 @@
 <template>
-  
   <header>
-    <h1>Task Tacker</h1>
+    <h1>{{ title }}</h1>
+    <Button text="Click Here" color="black" />
   </header>
-
 </template>
 
 <script>
-  export default {
-  name: "Header",
-  }
+  import Button from './Button'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+
+}
 </script>
 
 <style scoped>
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-  }
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
